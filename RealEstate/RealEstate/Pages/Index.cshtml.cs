bd3f074cc68d5848fb7492f13b00d8647128f2e0
kishoret7 <kishoretedu@gmail.com>
2021-11-08 22:21:42 -0500

    Index cshtml commit 2

--- RealEstate/RealEstate/Pages/Index.cshtml.cs
+++ RealEstate/RealEstate/Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LandData;
+using System.Net;
+using HouseData;
 
 namespace RealEstate.Pages
 {
@@ -19,6 +22,30 @@ namespace RealEstate.Pages
 
         public void OnGet()
         {
+
+
+            using (var webClient = new WebClient())
+            {
+                // grab our JSON text. 
+                var houseJSON = webClient.DownloadString("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
+
+                // convert raw text to list.
+                List<Houses> houseCollection = Houses.FromJson(houseJSON);
+
+
+                ViewData["Houses"] = houseCollection;
+
+                var landJSON = webClient.DownloadString("https://data.cityofchicago.org/resource/aksk-kvfp.json");
+
+                // convert raw text to list.
+                List<Lands> landCollection = Lands.FromJson(landJSON);
+
+
+                ViewData["Lands"] = landCollection;
+
+
+            }
+
 
         }
     }
